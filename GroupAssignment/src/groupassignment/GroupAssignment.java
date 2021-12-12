28a0c4eaab0230a50e81a5cf808978aa4b813212
LiDewong <95974704+LiDewong@users.noreply.github.com>
2021-12-13 00:00:21 +0800

    test push

--- GroupAssignment/src/groupassignment/GroupAssignment.java
+++ GroupAssignment/src/groupassignment/GroupAssignment.java
@@ -3,5 +3,7 @@ public class GroupAssignment {
     public static void main(String[] args) {
         System.out.println("Testing");
         System.out.println("Again");
+        System.out.println("I'm Li De, nice to meet you all");
+       
     }
 }
