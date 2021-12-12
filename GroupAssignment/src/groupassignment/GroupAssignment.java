17b78d8433c571baf8a8b65068da6b5cd7ca960e
kqrrrr <kangqiaorou@gmail.com>
2021-12-12 21:27:38 +0800

    nothing

--- GroupAssignment/src/groupassignment/GroupAssignment.java
+++ GroupAssignment/src/groupassignment/GroupAssignment.java
@@ -1,11 +1,7 @@
 package groupassignment;
-
 public class GroupAssignment {
-
     public static void main(String[] args) {
         System.out.println("Testing");
         System.out.println("Again");
-        System.out.println("Aligato dozaimas");
     }
-    
 }
